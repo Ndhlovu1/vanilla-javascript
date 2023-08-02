288b3986d0f8ce37c81c204f2e20a9076930a676
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-02 19:32:32 +0200

    Added Other Operators

--- 101Basics/data_types.js
+++ 101Basics/data_types.js
@@ -3,10 +3,27 @@
  */
 
 //ADDITION
-console.log("ADDITION")
+console.log("--ADDITION--")
 console.log(2+2)
 console.log(1 + 2 + 3 + 4)
 
+//SUBTRACTION 
+console.log("--SUBTRACTION--")
+console.log(20 - 18)
+console.log(15 - 35)
 
+//MULTIPLICATION
+console.log("--MULTIPLICATION--")
+console.log(10 * 20)
+console.log(30 * 40)
 
+//DIVISION
+console.log("--DIVISION--")
+console.log(20 / 5)
+console.log(1200 / 80)
 
+//COMPARISON OPERATORS
+console.log("--LOGICAL OPERATORS--")
+console.log(20 < 5)
+console.log(20 > 5)
+console.log(20 == 5)
