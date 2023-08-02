8009db976f93e2547b49a1075396f5d042142db4
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-03 01:31:22 +0200

    Basic Tasks with Logical Operators

---
+++ 101Basics/advanced_use_of_operators.js
@@ -0,0 +1,43 @@
+//ADVANCED USE OF LOGICAL && OPERATORS
+
+//Task 1
+var score = 8
+console.log("Mid-level Skills : ", score > 0 && score < 10)
+
+//Task 2
+var timeRemaining = 12
+var energy = 10
+
+console.log("Game Over :", timeRemaining == 0 || energy < 10)
+var num1 = 2
+var num2 = 5
+var test1 = num1 % 2
+var test2 = num2 % 2
+var result1 = test1 == 0
+var result2 = test2 == 0
+
+//Task 3
+console.log("Is",num1, "an even number?", result1)
+console.log("Is", num2, "an even number?", result2)
+
+//Task 4
+console.log("5 + 10 =",5 + 10)
+
+//Task 5
+var now = "Now in "
+var three = 3
+var d = "D!"
+console.log(now + three + d)
+
+//Task 6
+var counter = 0
+counter += 5
+counter += 3
+console.log("5 + 3 =",counter)
+
+
+
+
+
+
+
