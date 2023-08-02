edfd8407720256961cd6d521032b1a56bbea7b98
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-03 00:09:40 +0200

    Added Logical Operators

--- 101Basics/data_types.js
+++ 101Basics/data_types.js
@@ -2,28 +2,42 @@
  BASIC ARITHMETIC OPERATORS
  */
 
+ console.log()
+ console.log("---- OPERATORS IN JAVASCRIPT---- ")
+
+ console.log()
 //ADDITION
 console.log("--ADDITION--")
-console.log(2+2)
-console.log(1 + 2 + 3 + 4)
+console.log(" 2 + 2 = "+ 2 +2)
+console.log(" 1 + 2 + 3 + 4 = "+1 + 2 + 3 + 4)
 
+console.log()
 //SUBTRACTION 
 console.log("--SUBTRACTION--")
-console.log(20 - 18)
-console.log(15 - 35)
+console.log(" 20 - 18 =",20 - 18)
+console.log(" 15 +-35 =", 15 - 35)
 
+console.log()
 //MULTIPLICATION
 console.log("--MULTIPLICATION--")
 console.log(10 * 20)
 console.log(30 * 40)
 
+console.log()
 //DIVISION
 console.log("--DIVISION--")
 console.log(20 / 5)
 console.log(1200 / 80)
 
+console.log()
 //COMPARISON OPERATORS
 console.log("--LOGICAL OPERATORS--")
 console.log(20 < 5)
 console.log(20 > 5)
 console.log(20 == 5)
+
+console.log()
+//ADDITIONAL OPERATORS
+console.log("THE LOGICAL '&&' Operator ")
+var currentTime = 10
+console.log("TIME COMPARISON : ",currentTime > 9 && currentTime < 17)
