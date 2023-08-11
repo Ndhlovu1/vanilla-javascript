e687c588a6107810391c73f3e3c30d85e4cd48f5
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-11 12:05:12 +0200

    Function Array Builder

--- ArraysObjectsFunctions/array_objects.js
+++ ArraysObjectsFunctions/array_objects.js
@@ -1,8 +1,27 @@
 
-console.log("\n--Arrays are Objects--")
+console.log("\n--Arrays are Objects--");
 
-var fruits = []
-fruits.push("apple")
-fruits.push("mango")
-fruits.push('pineaple')
+var fruits = []; //new array
+
+fruits.push("apple");
+fruits.push("mango");
+fruits.push('pineaple');
+
+console.log(fruits);
+//Remove Pineapple
+fruits.pop('pineaple');
+//Load New Fruits
+console.log(fruits);
+
+//CREATE AN ARRAY FUNCTION
+
+function arrayBuilder(one, two, three, four){
+    var arr = [];
+
+    arr.push(one)
+    arr.push(two)
+    arr.push(three)
+
+    console.log("PROVIDED ITEMS : ",arr)
+}
 
