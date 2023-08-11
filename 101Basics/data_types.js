ea3ee26424753bb05af2e4c3bb27a919df7d5b6a
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-11 10:20:13 +0200

    Updated the datatyoes

--- 101Basics/data_types.js
+++ 101Basics/data_types.js
@@ -8,30 +8,35 @@
  console.log()
 //ADDITION
 console.log("--ADDITION--")
+console.log()
 console.log(" 2 + 2 = "+ 2 +2)
 console.log(" 1 + 2 + 3 + 4 = "+1 + 2 + 3 + 4)
 
 console.log()
 //SUBTRACTION 
 console.log("--SUBTRACTION--")
+console.log()
 console.log(" 20 - 18 =",20 - 18)
 console.log(" 15 +-35 =", 15 - 35)
 
 console.log()
 //MULTIPLICATION
 console.log("--MULTIPLICATION--")
+console.log()
 console.log(10 * 20)
 console.log(30 * 40)
 
 console.log()
 //DIVISION
 console.log("--DIVISION--")
+console.log()
 console.log(20 / 5)
 console.log(1200 / 80)
 
 console.log()
 //COMPARISON OPERATORS
 console.log("--LOGICAL OPERATORS--")
+console.log()
 console.log(20 < 5)
 console.log(20 > 5)
 console.log(20 == 5)
@@ -39,6 +44,7 @@ console.log(20 == 5)
 console.log()
 //AND AND OPERATORS
 console.log("THE LOGICAL '&&' Operator ")
+console.log()
 var currentTime = 10
 console.log("TIME COMPARISON : ",currentTime > 9 && currentTime < 17)
 console.log("True && True",true && true)
@@ -49,6 +55,7 @@ console.log("False && True", false && true)
 console.log()
 //ADDITIONAL OPERATORS
 console.log("THE LOGICAL 'OR' Operator ")
+console.log()
 var currentTime = 7
 console.log(currentTime < 9 || currentTime > 17)
 console.log("True and False :",true || false)
@@ -58,6 +65,7 @@ console.log("False and False  :",false || false)
 console.log()
 //ADDITIONAL OPERATORS
 console.log("THE LOGICAL 'NOT : !' Operator ")
+console.log()
 var petHungry = true
 console.log('Feeding the pet')
 console.log("Pet is hungry: ",!petHungry)
@@ -67,6 +75,7 @@ console.log("Pet now hungry:",petHungry)
 console.log()
 //ADDITIONAL OPERATORS
 console.log("THE 'MODULUS : %' Operator ")
+console.log()
 console.log("22 % 5 =", 22 % 5)
 console.log("10 % 2 =", 10 % 2)
 console.log("25 % 7 =", 25 % 7)
@@ -74,6 +83,7 @@ console.log("25 % 7 =", 25 % 7)
 console.log()
 //ADDITIONAL OPERATORS
 console.log("THE 'EQUALITY : ==' Operator ")
+console.log()
 console.log("5 == '5' :",5 == '5')
 console.log("25 == 24", 25 == 24)
 console.log("4 == 4 :", 4 == 4)
@@ -81,6 +91,7 @@ console.log("4 == 4 :", 4 == 4)
 console.log()
 //ADDITIONAL OPERATORS
 console.log("THE 'STRICT EQUALITY : ===' Operator ")
+console.log()
 console.log("5 === 5 :",5 === '5')
 console.log("25 === 25", 25 === 25)
 console.log("4 === 4 :", 4 == 4)
