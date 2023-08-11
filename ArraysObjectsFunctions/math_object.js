509827e40911c997e99ba07034c03407e8650984
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-11 12:20:41 +0200

    Math Update

---
+++ ArraysObjectsFunctions/math_object.js
@@ -0,0 +1,34 @@
+/*
+
+    BUILT IN MATH OBJECTS
+
+    NUMBER CONSTANTS
+
+        1. Math.PI - 3.14159
+        2. Math.E - 2.718
+        3. Math.LN2 - 0.693
+
+    ROUNDING METHODS
+
+        1. Math.ceil() - Rounds up to the closest Integer
+        2. Math.floor() - Rounds down to the closest Integer
+        3. Math.round() - Rounds up to the closest integer if the decimal is .5 or above, else rounds down
+        4. Math.trunc() - Trims the decimal, Leaves only the integer
+
+    ARITHMETIC AND CALCULUS METHODS 
+
+        Math.pow(2,3) - Calculates the 2 to the power of 3, the result is 8
+        Math.sqrt(16) - Calculates the square root of 16, 4
+        Math.cbrt(8)  - Finds the cube root of 8, the result is 2
+        Math.abs(-10) - Returns the absolute value, the result is 10
+        Math.log(), Math.log2(), Math.log10() - Logarithmic Methods
+        Math.min(8,10,23) - Returns the Minimun of all values 8
+        Math.max(9, 8, 7) - Returns the Maximum of all values 9
+
+*/
+
+
+
+
+
+
