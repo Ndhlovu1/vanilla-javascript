47b5e60ff5d9dbdae4547bd85a14c06e22ccb0e3
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-12 12:32:02 +0200

    Strings cheat sheet update

---
+++ ArraysObjectsFunctions/strings.js
@@ -0,0 +1,42 @@
+/*
+
+    DOWNLOAD BLITZSCALING BOOK FOR BUSINESS GROWTH
+    DOWNLOAD THE LEAN STARTUP
+
+*/
+
+var vegies = ['onion','spinach', 'cabbage']
+
+console.log("Length is : "+vegies.length)
+
+
+for(var i = 0; i < vegies.length; i++){
+    console.log(vegies[i])
+}
+
+var greeting = "Hellow"
+
+for(var i = 0; i < greeting.length; i++){
+    console.log(greeting[i])
+}
+
+//console.log(vegies.pop())
+//Strings are not arrays
+var greet = "Hello"
+var user = "Ndhlovu"
+
+console.log(greet +" "+ user)
+console.log(greet.concat(user))
+
+/*
+    STRING METHODS
+
+        1. charAt()         e.g greet.charAt(0)
+        2. concat()         e.g "Wor".concat("rld")
+        3. indexOf()        e.g "Happy-Birthday".indexOf('a')
+        4. lastIndexOf()    e.g "Happy-Birthday".lastIndexOf('a)
+        5. split()          e.g "Happy-Birthday".split("-")
+        6. toUpperCase()    e.g greet.toUpperCase()
+        7. toLowerCase()    e.g greet.toLowerCase()
+
+*/
