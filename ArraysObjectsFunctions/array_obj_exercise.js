d0248e4f36aec04df9c21ed6936322ada88fd784
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-12 12:30:43 +0200

    Array Object Exercise

---
+++ ArraysObjectsFunctions/array_obj_exercise.js
@@ -0,0 +1,29 @@
+/*
+
+    TASKS
+
+*/
+
+var clothes = []
+
+clothes.push("Suit")
+clothes.push("Tuxido")
+clothes.push("Jeans")
+clothes.push("Collared Shirt")
+clothes.push("Vests")
+
+clothes.pop("Vests")
+console.log(clothes)
+
+clothes.push("Sandals")
+console.log(clothes[2])
+
+var favCar = {}
+
+favCar.color = "Red"
+favCar.convertable = true
+console.log("\n--Favourite Car--\n", favCar)
+
+
+
+
