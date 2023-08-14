a9293d01fa715c2d9850ba80b95ee8c55bd2a464
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-14 12:52:26 +0200

    Key-Value Pairs with Methods

---
+++ ArraysObjectsFunctions/object_methods.js
@@ -0,0 +1,15 @@
+/*
+    KEY-VALUE PAIRS THAT CARRY FUNCTIONS AS VALUES
+*/
+
+//Declare an Object Literal
+var car = {}
+
+car.color = "Bright Red"
+car.turnkey = function() {
+    console.log("Starting Car ...")
+}
+
+console.log("--CAR OBJECT LITERAL--\n",car)
+
+
