35ba6bd8546ac9a16cbf12b7ca06f6c25b6f85d5
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-14 14:16:52 +0200

    Update for TypeOf

--- ArraysObjectsFunctions/typeOfOperator.js
+++ ArraysObjectsFunctions/typeOfOperator.js
@@ -12,5 +12,15 @@ var yes_or_no = true
 var comparison = 1 > 4
 var arr = [1,2,'name',9]
 var dict_object_literal = {names : 'Tinomudaishe Ndhlovu'}
+var func_test = function abc(){
+    console.log('abc')
+}
 
-
+console.log(sentence, " : ", typeof(sentence))
+console.log(int_num, " : ", typeof(int_num))
+console.log(amount, " : ", typeof(amount))
+console.log(yes_or_no, " : ", typeof(yes_or_no))
+console.log(comparison, " : ", typeof(comparison))
+console.log(arr, " : ", typeof(arr))
+console.log(dict_object_literal, " : ", typeof(dict_object_literal))
+console.log(func_test, " : ", typeof(func_test))
