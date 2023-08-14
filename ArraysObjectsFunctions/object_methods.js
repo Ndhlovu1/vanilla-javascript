18ba541a3d69a0750369f5763f4fc7745ee021b3
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-14 13:09:55 +0200

    Example

--- ArraysObjectsFunctions/object_methods.js
+++ ArraysObjectsFunctions/object_methods.js
@@ -3,13 +3,34 @@
 */
 
 //Declare an Object Literal
-var car = {}
+var car = {};
 
-car.color = "Bright Red"
-car.turnkey = function() {
-    console.log("Starting Car ...")
+//Normal K-P Values
+car.color = "Bright Red";
+car.mileage = "20 000km";
+
+
+//Object Method
+
+//Object Method
+car.price_details = function() {
+    var cost = 45000
+    var discount = 0.05
+    var discount_values = discount*cost
+    var final_price = cost - discount_values
+    car.name = "Porshe Panamera"
+
+    console.log("CAR NAME : ",car.name)
+    console.log("CAR COST : NAD "+cost)
+    console.log("CAR DISCOUNT : "+discount)
+    console.log("FINAL CAR COST : NAD "+final_price)
+    
+
 }
 
-console.log("--CAR OBJECT LITERAL--\n",car)
+
+console.log("--CAR OBJECT LITERAL--\n",car,"\n");
+
+car.price_details();
 
 
