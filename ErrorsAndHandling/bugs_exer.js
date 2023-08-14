7dd3745e839570fd2eff68f61dbb45c2c7beadcd
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-14 15:21:51 +0200

    Exercise

---
+++ ErrorsAndHandling/bugs_exer.js
@@ -0,0 +1,20 @@
+/*
+
+    TASK 1
+
+*/
+
+function addTwoNums(a, b){
+    /Add the function into a try-catch block
+try{
+    
+}
+
+
+    var sum = a + b
+    console.log(a+" + "+b+" =",sum)    
+}
+
+addTwoNums(5,5)
+
+
