8d665e7d82806d0f6b878927573e2690e6993dc3
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-14 14:12:50 +0200

    Update for TypeOf

--- ArraysObjectsFunctions/typeOfOperator.js
+++ ArraysObjectsFunctions/typeOfOperator.js
@@ -5,6 +5,12 @@
 console.log("TYPE OF CHECKS AND OUTPUTS")
 console.log("-------------------------\n")
 
-
+var sentence = "I am a sentence."
+var int_num = 44
+var amount = 44.10
+var yes_or_no = true
+var comparison = 1 > 4
+var arr = [1,2,'name',9]
+var dict_object_literal = {names : 'Tinomudaishe Ndhlovu'}
 
 
