063fe1d8b30bd40e1f9033ac1262b6990dc0b4f9
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-14 16:42:03 +0200

    Bugs exercise completed

--- ErrorsAndHandling/bugs_exer.js
+++ ErrorsAndHandling/bugs_exer.js
@@ -6,10 +6,30 @@
 
 function addTwoNums(a, b){
  
+     
+   try {
+
+        if(typeof(a) != 'number'){
+            throw new ReferenceError("Error : The first value isn't a number")
+        }
+        else if(typeof(a) != 'number'){
+            throw new ReferenceError("Error : The second value isn't a number")
+        }
+        else{
+            console.log(a+" + "+ b+" = ",a+b)
+        }
+
+   } catch (error) {
+        console.log("Erro!",error)
+   }
+
+ 
 
 
-    var sum = a + b
-    console.log(a+" + "+b+" =",sum)    
 }
 
 addTwoNums(5,5)
+addTwoNums("5",5)
+addTwoNums(8,9)
+
+console.log("It still works")
