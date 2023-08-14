fbc8a78992a25c75d558f961b05811abe1f96271
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-14 14:55:39 +0200

    Bug handling Update

--- ErrorsAndHandling/bugs.js
+++ ErrorsAndHandling/bugs.js
@@ -9,18 +9,37 @@ try{
 }
 catch(error){
     console.log("Error Identified")
-    console.log("------------------------")
-    console.log(error)
+    //console.log("------------------------")
+    //console.log(error)
     console.log("------------------------")
     console.log("Error has been saved to the error log")
 }
 
-console.log("#######################")
-console.log("Program Still Running")
 
 /*
     TYPES OF ERRORS
 
 */
 
+function syntax_logical_runtime_errors() {
+
+
+console.log("########################################################")
+
+console.log("######### TYPES OF ERRORS ##############")
+    
+    var ref_error = "REFERENCE ERROR : USED WHEN AN ATTEMPT TO USE A NON EXISTING VARIABLE IS MADE\ne.g.\n"
+    console.log(ref_error)
+    //console.log(username)
+    console.log()
+    var syn_error = "SYNTAX ERROR : OCCURS WHEN INVALID JAVASCRIPT IS USED\ne.g\n"
+    console.log(syn_error)
+    //var a "This is an error"
+    console.log("TypeError : WHen running the expected data type is different to the provided data type")
+
+
+
+
+
+}
 
