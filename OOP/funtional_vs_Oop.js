2b4acba72706bbeb9050519955046b22e1bfab7b
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-17 11:40:41 +0200

    Programming Paradigms

---
+++ OOP/funtional_vs_Oop.js
@@ -0,0 +1,16 @@
+//Funtional Programming Calculate price of shoes
+var shoes = 100;
+var stateTax = 1.2
+
+function totalprice(shoes, tax) {
+    return shoes * tax
+}
+
+var toPay = totalprice(shoes, stateTax)
+console.log(toPay)
+
+
+
+
+
+
