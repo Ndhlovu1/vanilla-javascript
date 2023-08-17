bdd6af65ea42733ab730635441920849fa80b940
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-17 11:40:08 +0200

    Encapsulation Usage

---
+++ OOP/encapsulation.js
@@ -0,0 +1,19 @@
+/*
+    ENCAPSULATION : HIDING A CODE IMPLEMENTATION FROM OTHER USERS, IN THE SENSE THAT THEY DONT NEED TO KNOW HOW TO IT WORKS IN ORDER TO USE IT
+
+    e.g. 
+        executing "tino".toUpperCase() doesnt require me to know how .toUpperCase() works 
+
+*/
+
+let user = "tinomudaishe ndhlovu".toUpperCase()
+//Even if the underlying implementation change, I wont have to worry about it as long as it doesnt break my code
+
+console.log(user)
+
+/**
+ * 
+ *  ABSTRACTION : EXTRACTING THE CONCEPT OF WHAT U WANNA DO AND WRITING CODE IN A WAY THAT WILL MAKE IT MORE GENERALIZED
+ * 
+ *  
+ */
