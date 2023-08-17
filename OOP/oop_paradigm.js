60d271984e478ae40a53a8e2f010e8dd34521fc1
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-17 11:46:01 +0200

    Update OOP

--- OOP/oop_paradigm.js
+++ OOP/oop_paradigm.js
@@ -12,3 +12,20 @@
 
 */
 
+//e.g. Class Creation
+class Animal{
+
+}
+// INSTANTIATION OF Class ANIMAL Uses the Object.create() method
+var myDog = Object.create(Animal)
+
+console.log(Animal)
+
+// A more common method of creating a class is to simply use the new className() method 
+// When the new keyword is used Js implicitly calls the Object.create()
+
+var myDog = new Animal()
+
+ 
+
+
