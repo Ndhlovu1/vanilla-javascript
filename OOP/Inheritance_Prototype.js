1b28502a18e3694d195fd13fa7a89c07c0639983
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-17 11:43:11 +0200

    Inheritance Prototype Creation

--- OOP/Inheritance_Prototype.js
+++ OOP/Inheritance_Prototype.js
@@ -10,3 +10,12 @@ var bird = {
 }
 
 
+// Object.create is used used when the superclass isnt a class - class syntax is preferred
+var eagle1 = Object.create(bird)
+console.log("Eagle1 :",eagle1)
+
+
+console.log("\nEagle1 can fly :", eagle1.hasWings)
+console.log("Eagle1 can fly :", eagle1.canFly)
+console.log("Eagle1 has feathers :", eagle1.hasFeathers)
+
