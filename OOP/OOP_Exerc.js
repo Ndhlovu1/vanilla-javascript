bf3e6c1afb8d40d03ac719abe164618b4df5c355
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-17 11:45:13 +0200

    OOP Exercise

--- OOP/OOP_Exerc.js
+++ OOP/OOP_Exerc.js
@@ -11,3 +11,19 @@ var purchase1 = {
 
 purchase1.totalPrice()
 
+// ex 2
+var purchase2 = {
+    tot_shoes: 50,
+    tot_shoeTax : 1.2,
+
+    totalPrice: function(){
+        var cal = this.tot_shoes * this.tot_shoeTax
+        console.log("Checkout Price : ", cal)
+    }
+}
+
+purchase2.totalPrice()
+
+
+
+
