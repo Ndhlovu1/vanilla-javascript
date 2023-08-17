f312aa4bf5934da1e5fe5e529c30679e1421a94f
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-17 11:43:27 +0200

    Inheritance Prototype Update

--- OOP/Inheritance_Prototype.js
+++ OOP/Inheritance_Prototype.js
@@ -19,3 +19,22 @@ console.log("\nEagle1 can fly :", eagle1.hasWings)
 console.log("Eagle1 can fly :", eagle1.canFly)
 console.log("Eagle1 has feathers :", eagle1.hasFeathers)
 
+
+// Let's create a water bird
+var skipper = Object.create(bird)
+
+skipper.canFly = false
+console.log("\nPengin Skipper :", skipper)
+console.log("\nPengin has wings :", skipper.hasWings)
+console.log("Pengin has feathers :", skipper.hasFeathers)
+console.log("Pengin Skipper can fly :", skipper.canFly)
+
+//let's create an Ostrich 
+let ostrich = Object.create(bird)
+
+ostrich.canFly = false
+console.log("\nOstrich :", ostrich)
+console.log("\nOstrich has wings :", ostrich.hasWings)
+console.log("Ostrich has feathers :", ostrich.hasFeathers)
+console.log("Ostrich Skipper can fly :", ostrich.canFly)
+
