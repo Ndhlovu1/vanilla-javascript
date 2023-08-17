ae12a026fad351bbf222187dc5ee1a0632053d85
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-17 11:42:51 +0200

    Inheritance Prototype Creation

---
+++ OOP/Inheritance_Prototype.js
@@ -0,0 +1,12 @@
+/*
+    PROTOTYPE : IS AN OBJECT THAT HOLDS PROPERTIES TO BE SHARED BY MULTIPLE OTHER OBJECTS
+    INHERITANCE
+*/
+
+var bird = {
+    hasWings : true,
+    canFly : true,
+    hasFeathers : true
+}
+
+
