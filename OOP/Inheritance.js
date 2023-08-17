2384abb76b4cf411cb48e0c4ca8e95aaf58de69f
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-17 11:43:52 +0200

    Inheritance Basics

---
+++ OOP/Inheritance.js
@@ -0,0 +1,15 @@
+/*
+    INHERITANCE : There is a super-class which has attributes that are inherited from by the sub-class. 
+    a.k.a parent-child relationship where the child inherits from the parent and the child can have other kids that also inherit from 
+    itself and from the main parent class
+
+    e.g
+
+        1. There is a base class of Animal
+        2. There is another class inheriting from Animal and the name of this class is Bird
+        3. There is another class inheriting from Bird and this class is called Eagle
+
+        NB: TO SHOWCASE THE RELATIONSHIP OF CLASSES IN Js WE USE THE KEYWORD extends
+            i.e. class Eagle extends Bird
+
+*/
