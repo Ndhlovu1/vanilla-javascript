4ad99eb9207dc6118e4d2f9e27565d15bc765636
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-17 11:47:14 +0200

    Polymorphism Usage

---
+++ OOP/polymorphic_example.js
@@ -0,0 +1,9 @@
+/** 
+ * 
+ *  e.g
+
+        1. There is a class called Bird
+        2. There is a class inheriting from Bird and this class is called Eagle
+        3.  There is a class inheriting from Bird and this class is called Penguin
+*/
+
