be5c5a246926a528950aecee77be4feb57050afc
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-19 16:45:33 +0200

    How to style the console

---
+++ StylingConsole/style.js
@@ -0,0 +1,22 @@
+/*
+
+    BASIC STYLING
+        NB: THE CONSOLE LOG HAS SEVERAL METHODS THAT CAN BE FINE TUNED WHEN LOGGING
+            METHODS OF LOGGING
+            1. debug
+            2. info
+            3. log
+            4. warn
+            5. error
+
+        NB: APPLYING CSS TO STYLE THE CONSOLE
+            1. Use the %c directive
+
+
+
+*/
+
+//Single Text Styles
+console.log("This is %cmy stylish message.",'color:yellow; background-color:blue;')
+
+
