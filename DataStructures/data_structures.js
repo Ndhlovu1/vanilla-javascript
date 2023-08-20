2021318581e639df1ad7b658f197df385e1b31a6
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-20 04:29:54 +0200

    Using and creating data structures

---
+++ DataStructures/data_structures.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ *      LEARNING HOW TO USE OBECTS, ARRAYS, MAPS, SETS
+ *  
+ *      METHODS THAT WORK ON ARRAYS
+ *          1. forEach
+ *          2. filter
+ *          3. map
+ * 
+ */
+
+
+
+
