e5ba9b45699469ad2e79d6c3fd65e0b2596c7dcc
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-20 03:25:27 +0200

    Update Car type

--- Destructuring/forOfLoops.js
+++ Destructuring/forOfLoops.js
@@ -16,8 +16,16 @@ sportsCar.speed = "fast"
 console.log("Sports Car Object :", sportsCar)
 
 
-//Using a For in method
-console.log("--For in Loop")
+//Using a For in methodnot reliable as it shows whats on the Prototype
+console.log("--For in Loop--")
 for(prop in sportsCar){
     console.log(prop)
 }
+
+//Loops only over values in the Object not the prototype
+console.log("\n--For of Loop--")
+for(item of Object.keys(sportsCar)){
+    console.log(prop+ " : "+ sportsCar[item])
+}
+
+ 
