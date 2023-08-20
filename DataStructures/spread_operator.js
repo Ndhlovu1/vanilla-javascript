8ed612e129fcaedb9ab135006e8aa0ef4f4d3791
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-20 05:19:05 +0200

    Spread Rest Update

--- DataStructures/spread_operator.js
+++ DataStructures/spread_operator.js
@@ -40,3 +40,15 @@ const greeting = "Hello";
 const arrayOfChars = [...greeting];
 console.log(arrayOfChars); //  ['H', 'e', 'l', 'l', 'o']
 
+//Copy either an object or an array into a separate one
+const car1 = {
+    speed: 200,
+    color: 'yellow'
+}
+const car2 = {...car1}
+
+car1.speed = 201
+
+console.log(car1.speed, car2.speed)
+
+
