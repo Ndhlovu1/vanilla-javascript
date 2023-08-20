053c58c302a0299ca55b42898b5ef808e28b8d25
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-20 05:10:10 +0200

    Basic Usage Syntax of the Spread Operator

--- DataStructures/spread_operator.js
+++ DataStructures/spread_operator.js
@@ -1,6 +1,10 @@
 /**
  * 
  *      SPREAD OPERATOR
- *          NB: Used with ...array_name
+ *          NB: Used with ...array_name to expand your options
+ * 
+ *      REST OPERATOR - DESTRUCTURING
+ *          NB: Used to limit the options at your disposal aka boxing by creating a subarray
+ *             
  * 
  */
