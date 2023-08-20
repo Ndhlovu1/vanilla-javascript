0f287e4fa37501241fc89c5c9b529acd3b40946a
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-20 04:42:26 +0200

    Update ds

--- DataStructures/data_structures.js
+++ DataStructures/data_structures.js
@@ -11,21 +11,29 @@
 
 
 //ForEach() Allows you to loop over each of it's members
-const fruits = ["kiwi","mango","apple","pear",]
+const fruits = ["kiwi","mango","apple","pear"]
 
 function appendIndex(fruit, index) {
-    console.log(`${index}.${fruit}`)
+    console.log(`${index}.${fruit}`);
     
 }
 fruits.forEach(appendIndex)
 
 //Often we pass the array item directly into the call like
-const vegies = ['onion','garlic','potato']
+const vegies = ['onion','garlic','potato'];
 vegies.forEach(
     function(vegie, index){
         console.log(`-> ${index}.${vegie}`)
     }
 )
+
+//filter() filters array based on a specific test
+const nums = [0,10,20,30,40,50,60];
+
+nums.filter(function(num){
+    return num > 30;
+})
+
 
 
 
