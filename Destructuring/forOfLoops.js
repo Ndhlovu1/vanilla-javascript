b56326a85a899ca7a09fd98992ce172d446df293
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-20 03:17:11 +0200

    Update Car type

---
+++ Destructuring/forOfLoops.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ *      Object.create() //Used to instantiate prototypes that are not classes
+ * 
+ * 
+ */
+
+const car = {
+    engine : true,
+    steering : true,
+    speed : "slow"
+}
+
+console.log(typeof(car))
+
+
