f7346094d41c511353c99192790485872d9be88f
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-20 02:53:54 +0200

    Looping over objects with arrays

---
+++ Destructuring/for_loop.js
@@ -0,0 +1,11 @@
+/*
+    FOR LOOP
+        1. CANT DIRECTLY WORK ON AN OBJECT BECAUSE OBJECTS ARE NOT ITERABLE
+        
+        HOWEVER ARRAYS ARE ITERABLE AND WE CAN USE THEM TO ITERATE OVER OBJECTS
+
+            NB: 
+                1. Object.keys() receives an object(the one we wanna loop over) as a parameter
+                2. 
+
+*/
