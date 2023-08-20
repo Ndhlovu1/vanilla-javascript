d30612e87f4e6c420a25dcab234a12e65f9fc043
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-20 03:34:34 +0200

    Template literals and Strings

--- Destructuring/template_literals.js
+++ Destructuring/template_literals.js
@@ -5,7 +5,19 @@
  */
 
 //DIFFERENCE BETWEEN A TEMPLATE LITERAL AND A REGULAR STRING
+let greet = "Hello"
+let place = "World"
 
+//Both values can be displayed using template literals
+console.log(`${greet} ${place}!`)
+
+//Template literals can span multiple lines which cant be done by the prior two string creation methods
+var test = `Hello
+Mr
+Ndhlovu
+`
+
+console.log(test)
 
 
 
