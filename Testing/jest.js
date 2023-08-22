cb628952d737d36b3ed77eb853b28483276200f5
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-22 02:21:06 +0200

    Perfoming Tests in Js

---
+++ Testing/jest.js
@@ -0,0 +1,15 @@
+/**
+ *      JAVASCRIPT TESTING 
+ * 
+ *      RED TEST : REFACTORING FAILED TESTS
+ *      GREEN TEST : REFACTORING TEST
+ * 
+ *  TDD :
+ *      1. Write a failing test
+ *      2. Re-write code to pass
+ *      3. Optimize code without changing the results
+ * 
+ */
+
+//Write a test with
+
