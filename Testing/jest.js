31900637ca295347fedd5681f7880d19e244ab0e
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-08-22 02:35:09 +0200

    Update the Cost and SPEED OF Testing

--- Testing/jest.js
+++ Testing/jest.js
@@ -17,6 +17,8 @@
  * 
  */
 
-console.log("----------TYPES OF TESTING SOFTWARE----------")
-var 
-
+console.log("----------TYPES OF TESTING SOFTWARE----------\n")
+var end2endTesting = "1.\tEnd to End Testing / e2e Testing\nSlowest Speed and most costly\n\n"
+var integrationTesting = "2.\tIntergration Testing\nAverage Speed and Expensive\n\n"
+var unit = "3.\tUnit Testing\nHighest Speed and Lowest Cost\n\n"
+console.log(end2endTesting,integrationTesting,unit)
